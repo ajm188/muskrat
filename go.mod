8b9099f6a8b0a17e276adbfc8f19d8b7aa701e16
Andrew Mason <amason@hey.com>
2025-02-02 19:53:38 -0500

    done
    Signed-off-by: Andrew Mason <amason@hey.com>

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/ajm188/muskrat
+
+go 1.23.2
---
+++ main.go
@@ -0,0 +1,73 @@
+package main
+
+import (
+	"encoding/csv"
+	"flag"
+	"fmt"
+	"io"
+	"log"
+	"os"
+	"strings"
+	"time"
+)
+
+func read(path string) error {
+	f, err := os.Open(path)
+	if err != nil {
+		return err
+	}
+	defer f.Close()
+
+	data, err := io.ReadAll(f)
+	if err != nil {
+		return err
+	}
+
+	var targetDates []time.Time
+
+	for i, line := range strings.Split(string(data), "\n") {
+		if line == "" {
+			continue
+		}
+
+		ts, err := time.Parse("Jan 2 3:04:05 PM MST", line)
+		if err != nil {
+			return fmt.Errorf("error parsing record %d: %w", i, err)
+		}
+
+		if ts.Month() == time.January || ts.Month() == time.February {
+			targetDates = append(targetDates, ts)
+		}
+	}
+
+	r := csv.NewReader(f)
+	r.LazyQuotes = true
+
+	var d time.Duration
+	for i := 0; i < len(targetDates)-1; i++ {
+		j := i + 1
+
+		gap := targetDates[j].Sub(targetDates[i])
+		fmt.Println(gap)
+		if gap > d {
+			d = gap
+		}
+	}
+
+	fmt.Println("largest gap", d)
+
+	return nil
+}
+
+// cat elonmusk.csv |  grep -E 'Jan|Feb' | awk -F, '{print $(NF-1) $NF}' | sed 's/"//g' | grep -E '^(Jan|Feb)' > timestamps.txt
+func main() {
+	flag.Parse()
+
+	if flag.NArg() < 1 {
+		flag.Usage()
+	}
+
+	if err := read(flag.Arg(0)); err != nil {
+		log.Fatal(err)
+	}
+}
